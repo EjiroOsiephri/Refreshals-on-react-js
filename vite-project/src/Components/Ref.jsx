7fb3c7480239b2ea7e4a06f7aa8717b8f3663a53
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-07-03 05:14:41 +0100

    Refreshing knowledge

---
+++ vite-project/src/Components/Ref.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const Ref = () => {
+  return (
+    <React.Fragment>
+      <input type="text" placeholder="Testing" />
+    </React.Fragment>
+  );
+};
+export default Ref;
