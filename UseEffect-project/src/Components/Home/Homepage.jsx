8f22c7b3f71ebb5b7f31a4d8d86704428af9a21e
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-07-04 21:58:26 +0100

    Setting up workspace

---
+++ UseEffect-project/src/Components/Home/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <React.Fragment>
+      <div className="home-container">
+        <input type="text" />
+        <input type="text" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Homepage;
---
+++ UseEffect-project/src/Components/UseEffects/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Navigation/navbar.scss";
+
+const Navigation = () => {
+  return (
+    <React.Fragment>
+      <div className="navbar">
+        <h1>A Typical page</h1>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Navigation;
