5b006bcba1a735110e8b821b6ad77244b9e539da
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-07-16 12:21:13 +0100

    Using React.memo

---
+++ UseEffect-project/Behind-the scenes/DemoOutput.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DemoOutput = (props) => {
+  return (
+    <>
+      <p>{props.show ? "hi there" : ""}</p>
+    </>
+  );
+};
+
+export default DemoOutput;
--- UseEffect-project/src/App.jsx
+++ UseEffect-project/src/App.jsx
@@ -2,20 +2,23 @@ import Navigation from "./Components/UseEffects/Navigation/Navigation";
 import AuthContext from "./Store/Context";
 import React, { useState } from "react";
 import Homepage from "./Components/Home/Homepage";
+import DemoOutput from "../Behind-the scenes/DemoOutput";
 
 function App() {
-  const [disabledBtn, setDisabledBtn] = useState(false);
+  const [paragraph, showParagraph] = useState(false);
+
+  const showParagraphFunction = () => {
+    showParagraph((prevValue) => !prevValue);
+  };
 
   return (
-    <AuthContext.Provider
-      value={{
-        disabledBtn: false,
-      }}
-    >
-      <Navigation></Navigation>
-      <Homepage disabled={disabledBtn} setDisabled={setDisabledBtn}></Homepage>
-      <button></button>
-    </AuthContext.Provider>
+    <>
+      <div>
+        <h1>Hi there!</h1>
+        <DemoOutput show={paragraph}></DemoOutput>
+        <button onClick={showParagraphFunction}>Show paragraph</button>
+      </div>
+    </>
   );
 }
 
