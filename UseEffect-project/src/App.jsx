a0a327ffb60878c5a07f6db2611c128924656fb7
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-07-16 22:12:10 +0100

    Class-base components

--- UseEffect-project/src/App.jsx
+++ UseEffect-project/src/App.jsx
@@ -1,16 +1,33 @@
 import Navigation from "./Components/UseEffects/Navigation/Navigation";
 import AuthContext from "./Store/Context";
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Homepage from "./Components/Home/Homepage";
 import DemoOutput from "../Behind-the scenes/DemoOutput";
 
 function App() {
   const [paragraph, showParagraph] = useState(false);
 
-  const showParagraphFunction = () => {
+  const showParagraphFunction = useCallback(() => {
     showParagraph((prevValue) => !prevValue);
-  };
+  }, []);
   console.log("App running");
+
+  function outerFunction() {
+    var outerVariable = "i am an outer variable";
+    function innerFunction() {
+      console.log(outerVariable);
+    }
+    return innerFunction;
+  }
+  const closure = outerFunction();
+  closure();
+
+  const numbers = [1, 2, 3, 4, 5];
+  const array = numbers.map((item) => {
+    return item * 2;
+  });
+  console.log(array);
+
   return (
     <>
       <div>
