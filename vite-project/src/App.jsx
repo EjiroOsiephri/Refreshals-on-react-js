3859fa1fac0962889b9d518a0f302b9f1fafbc80
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-07-20 17:06:58 +0100

    connecting to a database succesfully done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,36 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import MovieData from "./components/MovieData/MovieData";
+import "./App.scss";
 
 function App() {
-  return <Fragment></Fragment>;
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesData = () => {
+    fetch("https://swapi.dev/api/films/")
+      .then((res) => res.json())
+      .then((data) => {
+        const transFormedMovieHandler = data?.results.map((item) => {
+          return {
+            title: item.title,
+            id: item.episode_id,
+            openingText: item.opening_crawl,
+          };
+        });
+        setMovies(transFormedMovieHandler);
+      });
+  };
+  console.log(movies);
+
+  return (
+    <Fragment>
+      <section>
+        <button onClick={fetchMoviesData}>Fetch Movies</button>
+      </section>
+      <section>
+        <MovieData movies={movies}></MovieData>
+      </section>
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ vite-project/src/components/MovieData/MovieData.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Classes from "../../Sass/Movies.module.scss";
+
+const MovieData = (props) => {
+  return (
+    <>
+      {props.movies.map((item) => {
+        return (
+          <div className={Classes.movie} key={item.id}>
+            <h2>{item.title}</h2>
+            <h3>{item.openingText}</h3>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default MovieData;
