ce3859a6c30ae21b493c5623a8be2403724f5cc8
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-07-21 15:39:16 +0100

    Anew way to render dynamic data when the page loads with useCallback

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,45 @@
 import { Fragment, useState } from "react";
 import MovieData from "./components/MovieData/MovieData";
 import "./App.scss";
+import { useEffect } from "react";
+import { useCallback } from "react";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [loading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  async function fetchMoviesData() {
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
-    const transFormedMovieHandler = data?.results.map((item) => {
-      return {
-        title: item.title,
-        id: item.episode_id,
-        openingText: item.opening_crawl,
-      };
-    });
-    setMovies(transFormedMovieHandler);
-  }
-  console.log(movies);
+  const fetchMoviesData = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+
+      const transFormedMovieHandler = data?.results.map((item) => {
+        return {
+          title: item.title,
+          id: item.episode_id,
+          openingText: item.opening_crawl,
+        };
+      });
+
+      setMovies(transFormedMovieHandler);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setIsLoading(false);
+  });
+
+  useEffect(() => {
+    fetchMoviesData();
+  }, [fetchMoviesData]);
 
   return (
     <Fragment>
@@ -25,7 +47,9 @@ function App() {
         <button onClick={fetchMoviesData}>Fetch Movies</button>
       </section>
       <section>
-        <MovieData movies={movies}></MovieData>
+        {!loading && <MovieData movies={movies}></MovieData>}
+        {loading && <h2>Loading</h2>}
+        {!loading && error && <p>{error}</p>}
       </section>
     </Fragment>
   );
