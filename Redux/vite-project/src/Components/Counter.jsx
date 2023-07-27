4e1d7fe589fd153786fcaf2027354e0981b5f997
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-07-27 21:09:51 +0100

    an alternative to redux

--- Redux/vite-project/src/Components/Counter.jsx
+++ Redux/vite-project/src/Components/Counter.jsx
@@ -16,14 +16,18 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
-
   const counter = useSelector((state) => state.counter);
+
+  const toggle = useSelector((state) => state.showCounter);
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {toggle && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase</button>
