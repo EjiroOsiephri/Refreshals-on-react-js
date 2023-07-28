76d0c9b3e449308dbfc16c64767fb2f13a4348d7
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-07-28 08:58:09 +0100

    Updated header section

--- Redux/vite-project/src/Components/Header.jsx
+++ Redux/vite-project/src/Components/Header.jsx
@@ -3,29 +3,34 @@ import { useSelector, useDispatch } from "react-redux";
 import { authActions } from "../Store/Redux";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
   const logoutHandler = (e) => {
     e.preventDefault();
     dispatch(authActions.logout());
   };
 
-  const dispatch = useDispatch();
-  const authState = useSelector((state) => state.initialAuthState);
+  const authState = useSelector(
+    (state) => state.authentication.initialAuthState
+  );
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">My Products</a>
-          </li>
-          <li>
-            <a href="/">My Sales</a>
-          </li>
-          <li>
-            <button onClick={logoutHandler}>Logout</button>
-          </li>
-        </ul>
-      </nav>
+      {authState && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 };
