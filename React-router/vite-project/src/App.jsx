a9e557e8eb8ae8bf6a21402d00a708daaebcd51a
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-08-01 13:31:30 +0100

    Redirecting user to a not found page

--- React-router/vite-project/src/App.jsx
+++ React-router/vite-project/src/App.jsx
@@ -4,15 +4,18 @@ import Carts from "./Components/carts";
 import { Fragment } from "react";
 import Mainheader from "./Components/Mainheader";
 import CartDetail from "./Components/CartDetail";
+import Notfound from "./Components/Notfound";
 
 function App() {
   return (
     <Fragment>
       <Mainheader />
       <Routes>
+        <Route path="/"></Route>
         <Route path="/welcome/*" element={<Welcome />}></Route>
         <Route path="/carts" element={<Carts />}></Route>
         <Route path="/carts/:productId" element={<CartDetail />}></Route>
+        <Route path="*" element={<Notfound />}></Route>
       </Routes>
     </Fragment>
   );
