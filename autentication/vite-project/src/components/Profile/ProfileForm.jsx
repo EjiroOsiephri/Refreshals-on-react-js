83f19b60379ea6fc82651f17f85c0711903b42aa
EjiroOsiephri <osiephriejiro765@gmail.com>
2023-08-19 10:20:44 +0100

    Added change password logic

--- autentication/vite-project/src/components/Profile/ProfileForm.jsx
+++ autentication/vite-project/src/components/Profile/ProfileForm.jsx
@@ -1,17 +1,49 @@
-import classes from './ProfileForm.module.css';
+import { useContext, useRef } from "react";
+import classes from "./ProfileForm.module.css";
+import AuthContext from "../../Store/Auth-context";
 
 const ProfileForm = () => {
+  const ctx = useContext(AuthContext);
+
+  const changePasswordRef = useRef();
+
+  const handleChangePassword = async (e) => {
+    e.preventDefault();
+
+    const changedPasswordValue = changePasswordRef?.current?.value;
+
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCio_cSXnszom1TvYtWOM_SvPa_VXyhjgk",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            idToken: ctx.token,
+            password: changedPasswordValue,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleChangePassword}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input ref={changePasswordRef} type="password" id="new-password" />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
